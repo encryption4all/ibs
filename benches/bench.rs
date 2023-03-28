5f20ade434a7a8c25cb98040eb1dedc91b603859
Leon <l.botros@cs.ru.nl>
2023-03-28 14:45:18 +0200

    feat: benchmark throughput

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use ibs::{
     gg,
     gg::{Identity, Signer, Verifier},
@@ -10,40 +10,44 @@ pub fn criterion_benchmark_ibs(c: &mut Criterion) {
     let mut rng = thread_rng();
 
     let (pk, sk) = gg::setup(&mut rng);
-    let id = Identity::from("Johnny");
+    let id = Identity::from("Johny");
 
     let usk_id = gg::keygen(&sk, &id, &mut rng);
-    let sig = Signer::new()
-        .chain(b"The eagle has landed")
-        .sign(&usk_id, &mut rng);
+    let sig = Signer::new().chain(b"Some message").sign(&usk_id, &mut rng);
 
-    c.bench_function("setup", |b| {
-        let mut rng = thread_rng();
-        b.iter(|| gg::setup(&mut rng))
-    });
-
+    c.bench_function("setup", |b| b.iter(|| gg::setup(&mut rng)));
     c.bench_function("keygen", |b| {
-        let mut rng = thread_rng();
-        b.iter(|| gg::keygen(&sk, &id, &mut rng))
+        b.iter(|| gg::keygen(black_box(&sk), black_box(&id), &mut rng))
     });
 
-    c.bench_function("sign", |b| {
-        let mut rng = thread_rng();
+    let mut group = c.benchmark_group("GG sign/verify");
+    group.sample_size(10);
 
-        b.iter(|| {
-            Signer::new()
-                .chain(b"The eagle has landed")
-                .sign(&usk_id, &mut rng)
-        })
-    });
+    for l in (10..22).step_by(2) {
+        let size: u64 = 2u64.pow(l);
+        group.throughput(criterion::Throughput::Bytes(size));
 
-    c.bench_function("verify", |b| {
-        b.iter(|| {
-            Verifier::new()
-                .chain("The eagle has landed")
-                .verify(&pk, &sig, &id)
-        })
-    });
+        let mut msg = vec![0u8; size as usize];
+        rng.fill_bytes(&mut msg);
+
+        group.bench_function(format!("sign {size}"), |b| {
+            b.iter(|| {
+                Signer::new()
+                    .chain(black_box(&msg))
+                    .sign(black_box(&usk_id), &mut rng)
+            })
+        });
+
+        group.bench_function(format!("verify {size}"), |b| {
+            b.iter(|| {
+                Verifier::new().chain(black_box(&msg)).verify(
+                    black_box(&pk),
+                    black_box(&sig),
+                    black_box(&id),
+                )
+            })
+        });
+    }
 }
 
 criterion_group!(benches, criterion_benchmark_ibs);
